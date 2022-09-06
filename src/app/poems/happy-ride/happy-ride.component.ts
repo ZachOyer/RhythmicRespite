771240848817b814635189cdc6bb15a545d13860
Zach Oyer <zoyer@menard-inc.com>
2022-09-06 11:14:06 -0500

    Fixed music not stopping, added fade in/out

--- src/app/poems/happy-ride/happy-ride.component.ts
+++ src/app/poems/happy-ride/happy-ride.component.ts
@@ -18,10 +18,61 @@ export class HappyRideComponent implements OnInit {
   ngOnDestroy(): void {
     //Called once, before the instance is destroyed.
     //Add 'implements OnDestroy' to the class.
+    adjustVolume(this.audio, 0)
+    // Actually stop the music after it fades out
+    setTimeout(() => {
+      this.audio.pause();
+    }, 1500);
   }
 
   ngOnInit(): void {
-    this.audio.play()
+    setTimeout(() => {
+      this.audio.play();
+      this.audio.volume = 0;
+      adjustVolume(this.audio, 0.2);
+    }, 1000)
   }
 
+}
+
+export async function adjustVolume(
+  element: HTMLMediaElement,
+  newVolume: number,
+  {
+      duration = 1500,
+      easing = swing,
+      interval = 13,
+  }: {
+      duration?: number,
+      easing?: typeof swing,
+      interval?: number,
+  } = {},
+): Promise<void> {
+  const originalVolume = element.volume;
+  const delta = newVolume - originalVolume;
+
+  if (!delta || !duration || !easing || !interval) {
+      element.volume = newVolume;
+      return Promise.resolve();
+  }
+
+  const ticks = Math.floor(duration / interval);
+  let tick = 1;
+
+  return new Promise(resolve => {
+      const timer = setInterval(() => {
+          element.volume = originalVolume + (
+              easing(tick / ticks) * delta
+          );
+
+          if (++tick === ticks + 1) {
+              clearInterval(timer);
+              resolve();
+          }
+      }, interval);
+  });
+}
+
+export function swing(p: number) {
+  return 0.5 - Math.cos(p * Math.PI) / 2;
 }

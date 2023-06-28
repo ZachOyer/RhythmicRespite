d7be0b367b04529fe52e7ca6e78761d90c2ce25f
Zach Oyer <zoyer@menard-inc.com>
2023-06-28 13:04:45 -0500

    Changed intro background on mobile, condensed tinder component animations

--- src/app/app-animation.ts
+++ src/app/app-animation.ts
@@ -26,7 +26,7 @@ export const appAnimations =
         ], { optional: true }),
       ])
     ]),
-    transition('Poem => SecondPoem', [
+    transition('Poem => SecondPoem, SecondPoem => ThirdPoem', [
       query(':enter, :leave',
         style({ position: 'fixed',  width: '100%' }),
         { optional: true }),
@@ -39,11 +39,11 @@ export const appAnimations =
           query(':leave', [
             style({ transform: 'translateX(0%) translateY(0%) rotate(0deg)'}),
             animate('0.5s ease-in-out',
-            style({ transform: 'translateX(-100%) translateY(10%) rotate(-15deg)'}),)
+            style({ transform: 'translateX(-120%) translateY(10%) rotate(-15deg)'}),)
           ], { optional: true }),
         ])
     ]),
-    transition('SecondPoem => ThirdPoem', [
+    transition('SecondPoem => Poem, ThirdPoem => SecondPoem', [
       query(':enter, :leave',
         style({ position: 'fixed',  width: '100%' }),
         { optional: true }),
@@ -56,41 +56,7 @@ export const appAnimations =
           query(':leave', [
             style({ transform: 'translateX(0%) translateY(0%) rotate(0deg)'}),
             animate('0.5s ease-in-out',
-            style({ transform: 'translateX(-100%) translateY(10%) rotate(-15deg)'}),)
-          ], { optional: true }),
-        ])
-    ]),
-    transition('SecondPoem => Poem', [
-      query(':enter, :leave',
-        style({ position: 'fixed',  width: '100%' }),
-        { optional: true }),
-        group([
-          query(':enter', [
-            style({ 'z-index': '-1'}),
-            animate('0.5s ease-in-out',
-            style({ 'z-index': '-1'}))
-          ], { optional: true }),
-          query(':leave', [
-            style({ transform: 'translateX(0%) translateY(0%) rotate(0deg)'}),
-            animate('0.5s ease-in-out',
-            style({ transform: 'translateX(100%) translateY(10%) rotate(15deg)'}),)
-          ], { optional: true }),
-        ])
-    ]),
-    transition('ThirdPoem => SecondPoem', [
-      query(':enter, :leave',
-        style({ position: 'fixed',  width: '100%' }),
-        { optional: true }),
-        group([
-          query(':enter', [
-            style({ 'z-index': '-1'}),
-            animate('0.5s ease-in-out',
-            style({ 'z-index': '-1'}))
-          ], { optional: true }),
-          query(':leave', [
-            style({ transform: 'translateX(0%) translateY(0%) rotate(0deg)'}),
-            animate('0.5s ease-in-out',
-            style({ transform: 'translateX(100%) translateY(10%) rotate(15deg)'}),)
+            style({ transform: 'translateX(120%) translateY(10%) rotate(15deg)'}),)
           ], { optional: true }),
         ])
     ]),
